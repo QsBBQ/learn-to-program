1ae7f4682b9d6bb8b01c09ed605a438cf7e5a5de
Quinn Avila <hkdnow@yahoo.com>
2014-02-22 16:29:59 -0600

    first and second part of ch5

--- ch5.rb
+++ ch5.rb
@@ -8,3 +8,8 @@ middle = gets.chomp
 puts "What is your last name?"
 last = gets.chomp
 puts "It is nice to meet you %s %s %s"% [first, middle, last]
+
+puts "What is your favorite number?"
+number = gets.chomp
+new_number = number.to_i + 1
+puts '%s how about this new favorite number? %s' % [first, new_number]
