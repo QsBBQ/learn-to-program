0f01f3d96d023befaa25f0b0b8ba0e812e09ed9a
Quinn Avila <hkdnow@yahoo.com>
2014-02-22 15:33:01 -0600

    Finished ch2.rb

---
+++ ch2.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby env
+
+puts "Hours in a year", 365*24
+puts "Hours in a year #{365*24}"
+puts "Minutes in a decade", (10*365+2)*24*60
+puts "My age in seconds", 39*365*24*60*60
+puts "The authors age", 1160000000/60/60/24/365
