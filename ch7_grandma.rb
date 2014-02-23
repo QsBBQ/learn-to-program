aee42c0c72bf939c884ddc2a13126a22ef541c38
Quinn Avila <hkdnow@yahoo.com>
2014-02-23 16:08:05 -0600

    with 3 bye's

--- ch7_grandma.rb
+++ ch7_grandma.rb
@@ -4,13 +4,21 @@
 
 
 puts "talk with you grandma"
+count = 0
 while true
   my_speak = gets.chomp
   rdm_year = rand(1930..1950)
   her_speak = "NO, NOT SINCE #{rdm_year}"
 
   if my_speak == "BYE"
-    break
+    count = count + 1
+    if count == 3 
+      puts "BYE DEAR"
+      break
+    else
+      puts "You can't leave me"
+    
+  end
   elsif my_speak != my_speak.upcase
     puts "HUH?! SPEAK UP, SONNY!" 
   elsif my_speak == my_speak.upcase
