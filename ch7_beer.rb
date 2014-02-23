313f167f8a6b04268f13234a5677806380012f78
Quinn Avila <hkdnow@yahoo.com>
2014-02-23 15:17:55 -0600

    finished 99 bottles of beer

---
+++ ch7_beer.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby env
+#ch7_beer.rb
+#99 bottles of beer
+
+bottles = 99
+while bottles != 0
+  puts '#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.'
+  puts "Take one down and pass it around, #{bottles = bottles-1} bottles of beer on the wall."
+  end
+  puts "No more bottles of beer on the wall, no more bottles of beer."
+  puts "Got to the store and buy some more, 99 bottles of beer on the wall"
+
+
+
+
