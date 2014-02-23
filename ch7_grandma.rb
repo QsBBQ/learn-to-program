3ee8f7c83807511fb8c73b320061f1a8ee13796f
Quinn Avila <hkdnow@yahoo.com>
2014-02-23 15:55:41 -0600

    Grandma loop

---
+++ ch7_grandma.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby env
+#Chapter 7 Deaf Grandma
+
+
+
+puts "talk with you grandma"
+while true
+  my_speak = gets.chomp
+  rdm_year = rand(1930..1950)
+  her_speak = "NO, NOT SINCE #{rdm_year}"
+
+  if my_speak == "BYE"
+    break
+  elsif my_speak != my_speak.upcase
+    puts "HUH?! SPEAK UP, SONNY!" 
+  elsif my_speak == my_speak.upcase
+    puts her_speak
+    
+  end
+end
